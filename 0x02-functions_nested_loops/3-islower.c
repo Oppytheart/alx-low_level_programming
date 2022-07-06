b1e59d2b6177381eaa18f2435720c603c27af94f
Oppytheart <afolayanopeoluwa@gmail.com>
2022-07-06 16:05:20 -0700

    3. islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+imt r;
+r = test_islower('H');
+_putchar(r + '0');
+r = test_islower('o');
+_putchar(r + '0');
+r = test_islower(108)
+_putchar(r + '0');
+_putchar('\n');
+return (0);
+}
